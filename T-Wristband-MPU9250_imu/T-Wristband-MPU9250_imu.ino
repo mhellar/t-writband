0557f426fb546d1137c8f77dbb328cd76efe3197
Mark Hellar <mark.hellar@gmail.com>
2021-01-01 22:01:54 -0800

    first commit

---
+++ T-Wristband-MPU9250_imu/T-Wristband-MPU9250_imu.ino
@@ -0,0 +1,276 @@
+
+#include <Arduino.h>
+#include <pcf8563.h>
+#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+#include <SPI.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include "sensor.h"
+#include "esp_adc_cal.h"
+#include "ttgo.h"
+#include "charge.h"
+
+//  git clone -b development https://github.com/tzapu/WiFiManager.git
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
+// #define FACTORY_HW_TEST     //! Test RTC and WiFi scan when enabled
+// #define ARDUINO_OTA_UPDATE      //! Enable this line OTA update
+
+
+#ifdef ARDUINO_OTA_UPDATE
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#endif
+
+
+#define TP_PIN_PIN          33
+#define I2C_SDA_PIN         21
+#define I2C_SCL_PIN         22
+#define IMU_INT_PIN         38
+#define RTC_INT_PIN         34
+#define BATT_ADC_PIN        35
+#define VBUS_PIN            36
+#define TP_PWR_PIN          25
+#define LED_PIN             4
+#define CHARGE_PIN          32
+
+extern MPU9250 IMU;
+
+TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+PCF8563_Class rtc;
+WiFiManager wifiManager;
+
+char buff[256];
+bool rtcIrq = false;
+bool initial = 1;
+bool otaStart = false;
+
+uint8_t func_select = 0;
+uint8_t omm = 99;
+uint8_t xcolon = 0;
+uint32_t targetTime = 0;       // for next 1 second timeout
+uint32_t colour = 0;
+int vref = 1100;
+
+bool pressed = false;
+uint32_t pressedTime = 0;
+bool charge_indication = false;
+
+uint8_t hh, mm, ss ;
+
+void configModeCallback (WiFiManager *myWiFiManager)
+{
+    Serial.println("Entered config mode");
+    Serial.println(WiFi.softAPIP());
+    //if you used auto generated SSID, print it
+    Serial.println(myWiFiManager->getConfigPortalSSID());
+
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE);
+    tft.drawString("Connect hotspot name ",  20, tft.height() / 2 - 20);
+    tft.drawString("configure wrist",  35, tft.height() / 2  + 20);
+    tft.setTextColor(TFT_GREEN);
+    tft.drawString("\"T-Wristband\"",  40, tft.height() / 2 );
+
+}
+
+void scanI2Cdevice(void)
+{
+    uint8_t err, addr;
+    int nDevices = 0;
+    for (addr = 1; addr < 127; addr++) {
+        Wire.beginTransmission(addr);
+        err = Wire.endTransmission();
+        if (err == 0) {
+            Serial.print("I2C device found at address 0x");
+            if (addr < 16)
+                Serial.print("0");
+            Serial.print(addr, HEX);
+            Serial.println(" !");
+            nDevices++;
+        } else if (err == 4) {
+            Serial.print("Unknow error at address 0x");
+            if (addr < 16)
+                Serial.print("0");
+            Serial.println(addr, HEX);
+        }
+    }
+    if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+    else
+        Serial.println("Done\n");
+}
+
+void setupWiFi()
+{
+#ifdef ARDUINO_OTA_UPDATE
+    WiFiManager wifiManager;
+    //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+    wifiManager.setAPCallback(configModeCallback);
+    wifiManager.setBreakAfterConfig(true);          // Without this saveConfigCallback does not get fired
+    wifiManager.autoConnect("T-Wristband");
+#endif
+}
+
+void setupOTA()
+{
+#ifdef ARDUINO_OTA_UPDATE
+    // Port defaults to 3232
+    // ArduinoOTA.setPort(3232);
+
+    // Hostname defaults to esp3232-[MAC]
+    ArduinoOTA.setHostname("T-Wristband");
+
+    // No authentication by default
+    // ArduinoOTA.setPassword("admin");
+
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+            type = "sketch";
+        else // U_SPIFFS
+            type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+        otaStart = true;
+        tft.fillScreen(TFT_BLACK);
+        tft.drawString("Updating...", tft.width() / 2 - 20, 55 );
+    })
+    .onEnd([]() {
+        Serial.println("\nEnd");
+        delay(500);
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+        // Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        int percentage = (progress / (total / 100));
+        tft.setTextDatum(TC_DATUM);
+        tft.setTextPadding(tft.textWidth(" 888% "));
+        tft.drawString(String(percentage) + "%", 145, 35);
+        drawProgressBar(10, 30, 120, 15, percentage, TFT_WHITE, TFT_BLUE);
+    })
+    .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+
+        tft.fillScreen(TFT_BLACK);
+        tft.drawString("Update Failed", tft.width() / 2 - 20, 55 );
+        delay(3000);
+        otaStart = false;
+        initial = 1;
+        targetTime = millis() + 1000;
+        tft.fillScreen(TFT_BLACK);
+        tft.setTextDatum(TL_DATUM);
+        omm = 99;
+    });
+
+    ArduinoOTA.begin();
+#endif
+}
+
+
+void setupADC()
+{
+    esp_adc_cal_characteristics_t adc_chars;
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
+    //Check type of calibration value used to characterize ADC
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
+        vref = adc_chars.vref;
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        Serial.printf("Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
+    } else {
+        Serial.println("Default Vref: 1100mV");
+    }
+}
+
+void setup(void)
+{
+    Serial.begin(115200);
+
+    tft.init();
+    tft.setRotation(1);
+    tft.setSwapBytes(true);
+    tft.pushImage(0, 0,  160, 80, ttgo);
+
+    Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+    Wire.setClock(400000);
+
+#ifdef FACTORY_HW_TEST
+    factoryTest();
+#endif
+
+    tft.fillScreen(TFT_BLACK);
+
+    tft.setTextColor(TFT_YELLOW, TFT_BLACK); // Note: the new fonts do not draw the background colour
+
+    targetTime = millis() + 1000;
+
+    pinMode(TP_PIN_PIN, INPUT);
+    //! Must be set to pull-up output mode in order to wake up in deep sleep mode
+    pinMode(TP_PWR_PIN, PULLUP);
+    digitalWrite(TP_PWR_PIN, HIGH);
+
+    pinMode(LED_PIN, OUTPUT);
+
+    pinMode(CHARGE_PIN, INPUT_PULLUP);
+    attachInterrupt(CHARGE_PIN, [] {
+        charge_indication = true;
+    }, CHANGE);
+
+    if (digitalRead(CHARGE_PIN) == LOW) {
+        charge_indication = true;
+    }
+}
+
+
+
+void IMU_Show()
+{
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextDatum(TL_DATUM);
+    readMPU9250();
+    snprintf(buff, sizeof(buff), "--  ACC  GYR   MAG");
+    tft.drawString(buff, 0, 0);
+    snprintf(buff, sizeof(buff), "x %.2f  %.2f  %.2f", (int)1000 * IMU.ax, IMU.gx, IMU.mx);
+    tft.drawString(buff, 0, 16);
+    snprintf(buff, sizeof(buff), "y %.2f  %.2f  %.2f", (int)1000 * IMU.ay, IMU.gy, IMU.my);
+    tft.drawString(buff, 0, 32);
+    snprintf(buff, sizeof(buff), "z %.2f  %.2f  %.2f", (int)1000 * IMU.az, IMU.gz, IMU.mz);
+    tft.drawString(buff, 0, 48);
+    delay(200);
+}
+
+
+void loop()
+{
+#ifdef ARDUINO_OTA_UPDATE
+    ArduinoOTA.handle();
+#endif
+
+    //! If OTA starts, skip the following operation
+    if (otaStart)
+        return;
+
+    if (charge_indication) {
+        charge_indication = false;
+        if (digitalRead(CHARGE_PIN) == LOW) {
+            tft.pushImage(140, 55, 16, 16, charge);
+        } else {
+            tft.fillRect(140, 55, 16, 16, TFT_BLACK);
+        }
+    }
+
+        IMU_Show();
+
+}
